35f628a98836c9837b26b2fa246bfa7e6bab755a
Johanderson <johalternate@gmail.com>
2024-04-17 10:09:16 -0400

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.APP_PLACEHOLDER_NAME.app',
+  appName: 'APP_PLACEHOLDER_NAME',
+  webDir: 'dist/APP_PLACEHOLDER_NAME/browser'
+};
+
+export default config;
